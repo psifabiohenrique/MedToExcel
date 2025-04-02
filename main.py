04649d9b43ed7ee348231e09568922a642731001
Fábio Henrique <psifabiohenrique@outlook.com>
2025-04-02 19:00:24 -0300

    Atualizado a versão do Python para 3.11.8, removidos arquivos de experimentos antigos e adicionado novo arquivo de configuração do projeto com dependências necessárias.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
 import sys
-import time
 from qdarktheme import load_stylesheet
 
 from MainWindow import MainWindow
 
 
 app = QApplication(sys.argv)
-app.setStyleSheet(load_stylesheet('dark'))
+app.setStyleSheet(load_stylesheet("dark"))
 app.setWindowIcon(QIcon("icone.png"))
 window = MainWindow()
 window.show()
